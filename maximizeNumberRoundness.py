85eb6abc880158e830fc67ada1e2639dae564186
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-04-07 18:10:28 +0200

    Create maximizeNumberRoundness.py

---
+++ maximizeNumberRoundness.py
@@ -0,0 +1,13 @@
+def maximizeNumberRoundness(n):
+    tmp = n
+    zeros = 0
+    while tmp:
+        if tmp % 10 == 0:
+            zeros += 1
+        tmp //= 10
+    result = zeros
+    for i in range(zeros):
+        if n % 10 == 0:
+            result -= 1
+        n //= 10
+    return result
