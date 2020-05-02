f253f8290ebbfd6fd7364aa744de01b664d10096
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-05-02 21:41:37 +0200

    Update growingPlant.py

--- growingPlant.py
+++ growingPlant.py
@@ -4,7 +4,6 @@ def growingPlant(upSpeed, downSpeed, desiredHeight):
     while height < desiredHeight:
         height += upSpeed
         days += 1
-        if height >= desiredHeight:
-            return days
+        if height >= desiredHeight: return days
         height -= downSpeed
     return days
