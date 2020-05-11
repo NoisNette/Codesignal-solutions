02fd739238deeb1557ed948d096eedd30411ca99
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-05-11 21:11:03 +0200

    Create boxPiles.py

---
+++ boxPiles.py
@@ -0,0 +1,15 @@
+def boxPiles(a):
+    a = sorted(a)
+    used = [False for j in range(len(a))]
+    used_num = 0
+    ans = 0
+    while used_num < len(used):
+        height = 0
+        for i in range(len(a)):
+            if a[i] >= height and not used[i]:
+                height+=1
+                used[i] = True
+                used_num += 1
+        ans+=1
+
+    return ans
