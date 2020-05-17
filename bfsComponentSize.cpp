693a3ebfd698c6675e7361b0736df06ba5addc44
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-05-17 21:55:35 +0200

    Create bfsComponentSize.cpp

---
+++ bfsComponentSize.cpp
@@ -0,0 +1,20 @@
+int bfsComponentSize(std::vector<std::vector<bool>> matrix) {
+    using namespace std;
+    vector<int> nodes{1};
+    set<int> visited;
+    while (!nodes.empty()) {
+        vector<int> next_level;
+        for (auto node: nodes) {
+            visited.insert(node);
+            for (int j = 0; j < matrix.size(); j++) {
+                if (matrix[node][j]) {
+                    if (visited.find(j) == visited.end()) {
+                        next_level.push_back(j);
+                    }
+                }
+            }
+        }
+        nodes = next_level;
+    }
+    return visited.size();
+}
