d6da4bbb1c84ed9a4b8eb0606ee70862b2cac09f
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-05-22 13:56:36 +0200

    Update validTime.py

--- validTime.py
+++ validTime.py
@@ -1,12 +1,3 @@
-bool validTime(string time) {
-    using namespace std;
-    regex reg("^(\\d\\d):(\\d\\d)$");
-    smatch matches;
-    regex_search(time, matches, reg);
-    if (matches.size() != 3) {
-        return false;
-    }
-    int h = stoi(matches[1].str());
-    int m = stoi(matches[2].str());
-    return 0 <= h && h <= 23 && 0 <= m && m <= 59;
-}
+def validTime(time):
+    h,m = map(int, time.split(":"))
+    return (0 <= h <= 23 and 0 <= m < 60) or (h == 23 and 0 <= m < 60)
