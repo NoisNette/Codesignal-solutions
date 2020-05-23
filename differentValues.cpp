432e0d261ab41424c34f0fd26514ef085113b8a8
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-05-23 13:08:22 +0200

    Update differentValues.cpp

--- differentValues.cpp
+++ differentValues.cpp
@@ -1,20 +1,11 @@
-int differentValues(std::vector<int> a, int d) {
-    int n = a.size();
-    int best = -1;
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            int diff = abs(a[i] - a[j]);
-            if (diff <= d) {
-                if (best == -1) {
-                    best = diff;
-                } else {
-                    best = max(best, diff);
-                }
-                if (best == d) {
-                    break;
-                }
-            }
-        }
-    }
-    return best;
-}
+def differentValues(a, d):
+    n = len(a)
+    best = -1
+    for i in range(n):
+        for j in range(i + 1, n):
+            diff = abs(a[i] - a[j])
+            if diff <= d:
+                if best == -1: best = diff
+                else: best = max(best, diff)
+                if best == d: break
+    return best
