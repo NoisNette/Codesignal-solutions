061e0652a52a9f071dec7a993c6bc630b2d076fe
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-05-23 13:05:22 +0200

    Create digitCharactersSum.py

---
+++ digitCharactersSum.py
@@ -0,0 +1,7 @@
+def digitCharactersSum(ch1, ch2):
+    x1 = ord(ch1) - ord('0')
+    x2 = ord(ch2) - ord('0')
+    if x1 + x2 < 10:
+        return chr(ord('0') + x1 + x2)
+    else:
+        return '1' + chr(ord('0') + (x1 + x2) % 10)
