f63ec84fb2abff4c5085ed2a4d8ac660813c1836
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-05-26 10:05:19 +0200

    Create insertBits.cpp

---
+++ insertBits.cpp
@@ -0,0 +1,8 @@
+int insertBits(int n, int a, int b, int k) {
+    int res = n;
+    for( int i = a ; i <= b ; ++i ){
+        int bit = k & 1<<(i - a);
+        res ^= (res ^ -bit) & (1<<i);
+    }
+    return res;
+}
