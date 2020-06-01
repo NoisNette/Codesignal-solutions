fbc798f4bb12204edcabf839977d5dc805368b45
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-06-01 08:36:56 +0200

    Update switchLights.py

--- switchLights.py
+++ switchLights.py
@@ -3,8 +3,5 @@ def switchLights(a):
     for i in range(1, len(a)):
         if a[i] == 1:
             for j in range(i, -1, -1):
-                if a[j] == 1:
-                    a[j] = 0
-                else:
-                    a[j] = 1
+                a[j] = int(not a[j])
     return a
