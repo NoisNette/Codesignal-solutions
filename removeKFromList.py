228f360efc02f4b6a4a0ba69ec8ce4c504f4124d
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-06-09 17:55:59 +0200

    Update and rename removeKFromList.cpp to removeKFromList.py

---
+++ removeKFromList.py
@@ -0,0 +1,8 @@
+def removeKFromList(l, k):
+    c = l
+    while c:
+        if c.next and c.next.value == k:
+            c.next = c.next.next
+        else:
+            c = c.next
+    return l.next if l and l.value == k else l
