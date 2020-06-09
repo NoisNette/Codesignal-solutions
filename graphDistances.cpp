5b9a52166dd7ad0857d10fdc2a801ed8f44b7ab4
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-06-09 09:37:37 +0200

    Create graphDistances.cpp

---
+++ graphDistances.cpp
@@ -0,0 +1,32 @@
+#define INF 1<<30
+#define pii pair<int,int>
+#define mp make_pair
+
+struct comp{
+    bool operator()(pii &p1, pii &p2){
+        return p1.second > p2.second;
+    }
+};
+
+vector<int> graphDistances(vector<vector<int>> g, int s) {
+    using namespace std;
+    int V = g.size();
+    vector<int> dist(V, INF);
+    dist[s] = 0;
+    priority_queue<pii, vector<pii>, comp> Q;
+    Q.push(mp(s, 0));
+
+    while (!Q.empty()){
+        pii p = Q.top(); Q.pop();
+        int cur = p.first;
+        for (int i = 0 ; i < V; ++i){
+            if (g[cur][i] >= 0) {
+                if (dist[i] > dist[cur] + g[cur][i]) {
+                    dist[i] = dist[cur] + g[cur][i];
+                    Q.push(mp(i, dist[i]));
+                }
+            }
+        }
+    }
+    return dist;
+}
