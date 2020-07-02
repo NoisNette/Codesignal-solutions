981e86bd79f52db6b390c30616e6881a47224c66
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-07-03 00:04:52 +0200

    Update boxPiles.py

--- boxPiles.py
+++ boxPiles.py
@@ -7,9 +7,8 @@ def boxPiles(a):
         height = 0
         for i in range(len(a)):
             if a[i] >= height and not used[i]:
-                height+=1
+                height += 1
                 used[i] = True
                 used_num += 1
-        ans+=1
-
+        ans += 1
     return ans
