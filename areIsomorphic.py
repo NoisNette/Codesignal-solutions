e17ba893a71ce3f33e3500e4eac20ee1ccb7fd52
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-07-04 12:47:31 +0200

    Update areIsomorphic.py

--- areIsomorphic.py
+++ areIsomorphic.py
@@ -1,5 +1,5 @@
 def areIsomorphic(array1, array2):
-    if len(array1) != len(arr2ay): return False
+    if len(array1) != len(array2): return False
     for i in range(len(array1)):
         if len(array1[i]) != len(array2[i]): return False
     return True
