232bc090aae493fdb5a5fcadede90f51d75511a3
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-07-11 12:58:20 +0200

    Update rightmostRoundNumber.py

--- rightmostRoundNumber.py
+++ rightmostRoundNumber.py
@@ -1,8 +1,4 @@
-def rightmostRoundNumber(inputArray):
-
-    ans = -1
-    for i in range(len(inputArray)):
-        if inputArray[i] % 10 == 0:
-            ans = i
-
-    return ans
+def rightmostRoundNumber(arr):
+    for i in range(len(arr)-1, -1, -1):
+        if arr[i] % 10 == 0: return arr.index(arr[i])
+    return -1
