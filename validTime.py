d7bf35a26927de3cab4caf5a8147b82f488364de
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-07-25 13:46:44 +0200

    Update validTime.py

--- validTime.py
+++ validTime.py
@@ -1,3 +1,3 @@
 def validTime(time):
-    h,m = map(int, time.split(":"))
-    return (0 <= h <= 23 and 0 <= m < 60) or (h == 23 and 0 <= m < 60)
+    h, m = map(int, time.split(":"))
+    return 0 <= h <= 23 and 0 <= m < 60
