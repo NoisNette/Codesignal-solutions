f41c2669604d7c3f5354b6b0b5ed51e67abba535
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-08-04 12:35:32 +0200

    Update fractionSubtraction.py

--- fractionSubtraction.py
+++ fractionSubtraction.py
@@ -1,14 +1,8 @@
 def fractionSubtraction(a, b):
-
-    def gcdEuclid(a, b):
-        if a == 0:
-            return b
-        return gcdEuclid(b % a, a)
-
     c = [a[0] * b[1] - a[1] * b[0], a[1] * b[1]]
-    gcd = gcdEuclid(c[0], c[1])
+    g = math.gcd(c[0], c[1])
 
-    c[0] /= gcd
-    c[1] /= gcd
+    c[0] /= g
+    c[1] /= g
 
     return c
