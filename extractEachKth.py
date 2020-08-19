2d1f518e4ba867a7fbd204974da13611f5581442
NoisNette <59642044+NoisNette@users.noreply.github.com>
2020-08-19 12:07:58 +0200

    Update extractEachKth.py

--- extractEachKth.py
+++ extractEachKth.py
@@ -1,6 +1,10 @@
 def extractEachKth(inputArray, k):
-    l = []
-    for i in range(1, len(inputArray)+1):
-        if (i) % k != 0:
-            l.append(inputArray[i-1])
-    return l
+    return [inputArray[i - 1] for i in range(1, len(inputArray) + 1) if i % k]
+
+
+# def extractEachKth(inputArray, k):
+#     l = []
+#     for i in range(1, len(inputArray)+1):
+#         if (i) % k != 0:
+#             l.append(inputArray[i-1])
+#     return l
